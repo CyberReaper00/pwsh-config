9c895f3db416a22a00ebf894507116004dfbe744
CyberReaper00 <nahyanl63@gmail.com>
2025-01-02 17:33:15 +0500

    added new functions and renamed some previous functions for better readability

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,105 +1,123 @@
+$null = Get-Command
+Write-Host "Some text is written" -Background Green
+
 Set-Alias seal Set-Alias
-seal rmi Remove-Item
+seal rmit Remove-Item
 seal gloc Get-Location
 seal show Get-ChildItem
 seal rnit Rename-Item
 seal fn function
 
 function cloc {
-  Get-Location | Select-Object -ExpandProperty Path | clip
+    Get-Location | Select-Object -ExpandProperty Path | clip
 }
 
-function fldr {
-  show -directory
+function lsf {
+    show -directory
 }
 
 function codes {
-  cd 'C:\users\windows 11\documents\code'
-  ls
+    cd 'C:\users\windows 11\documents\code'
+	ls
 }
 
 function qwe {
-  exit
+    exit
 }
 
 function lad {
-  cd $env:LOCALAPPDATA
+    cd $env:LOCALAPPDATA
 }
 
 function nconf {
-  cd 'C:\users\windows 11\appdata\local\nvim'
-  nvim init.lua
+    cd 'C:\users\windows 11\appdata\local\nvim'
+	nvim init.lua
 }
 
 function nplgn {
-  cd 'C:\tools\neovim\nvim-win64\share\nvim\runtime\plugin'
-  ls
+    cd 'C:\tools\neovim\nvim-win64\share\nvim\runtime\plugin'
+	ls
 }
 
 function prfl {
-  nvim $PROFILE
+    nvim $PROFILE
+}
+function cmtprfl {
+    cd 'C:\Users\Windows 11\documents\windowspowershell'
+    gadd
+    gcomm
+    gpm
 }
 
 function navs {
-  nvim .
+    nvim .
 }
 
 function iop {
-  explorer .
+    explorer .
 }
 
 function hm {
-  cd ~/
+    cd ~/
 }
 
 function newit {
-  param (
-    [string]$Name
-  )
-  New-Item -Path . -Name $Name -ItemType "File"
+    param (
+	    [string]$Name
+	  )
+	New-Item -Path . -Name $Name -ItemType "File"
 }
 
 function gadd {
-  param (
-    [string[]]$Files = @('.') # Default value
-  )
-  git add $Files
+    param (
+	    [string[]]$Files = @('.') # Default value
+	  )
+	git add $Files
 }
 
 function gcomm {
-  param (
-    [string[]]$Message = @('New commit') # Default value
-  )
-  git commit -m $Message
+    param (
+	    [string[]]$Message = @('New commit') # Default value
+	  )
+	$Message = Read-Host 'Enter Commit Message'
+	git commit -m $Message
 }
 
 function gss {
-  git status
+    git status
 }
 
 function gpm {
-  git push -u origin main
+    git push -u origin main
 }
 
 function psrvr {
-  param (
-    [string[]]$Port = @('8000') # Default value
-  )
-  python -m http.server $Port
+    param (
+	    [int]$Port = 8000 # Default value
+	  )
+	python -m http.server $Port
 }
 
 function chart {
-  cd 'C:\Users\Windows 11\documents\veracity files\st chart'
+    cd 'C:\Users\Windows 11\documents\veracity files\st chart'
 }
 
 function vfiles {
-  cd 'C:\users\windows 11\documents\veracity files'
-  fldr
+    cd 'C:\users\windows 11\documents\veracity files'
+	lsf
 }
 
-function atlcsv {
-  Param (
-    [string[]]$Port = @('8000')
-  )
-  cloudflared tunnel --url localhost:$Port
+function lcltnl {
+    Param (
+	    [int]$Port = 8000
+	  )
+	cloudflared tunnel --url localhost:$Port
+}
+
+function lsfN {
+    Param (
+	    [int]$Index = 1
+	  )
+	$Dir = Get-ChildItem -Path . -Directory | Select-Object -Skip ($Index-1) -First 1
+	cd $Dir
 }
